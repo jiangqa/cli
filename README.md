f6eee9d05c4e522f66ed87984ef22c616ac5b2af
蒋求安 <jiangqiuan@xianfengsg.com>
2022-02-19 22:56:13 +0800

    feat

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-cli 工具
+cli 工具 对内
 ```
-   npm install -g xf-vue-cli
+   npm install -g xf-vue-cli //安装
 ```
+
 ```
-  xf-vue-cli init <project-name>
+  xf-vue-cli project //拉取项目，维护常用项目，省去查找项目地址的烦恼
+  xf-vue-cli init <project-name> //创建新项目，admin,h5 2套模版
+  xf-vue-cli page <name> <path> //新建page vue页面并添加到路由
+  xf-vue-cli component <name> <path> //新建component组件
 ```
+
--- bin/xf-vue-cli.js
+++ bin/xf-vue-cli.js
@@ -4,6 +4,16 @@ const action = require('../lib/init')
 const inquirer = require('inquirer')
 const insertRouter = require('../lib/insert-router')
 const insertComponent = require('../lib/insert-component')
+const project = {
+    '供应链': {'基地erp-admin': '', '基地erp-h5': '', '基地erp-service': ''},
+    'POS': {'POS-admin': '', 'POS-iot': '', 'POS-service': '', 'POS-Android': ''},
+    '数据':{'数据门户-admin':'git@codeup.aliyun.com:xianfengsg/datagroup/data-portal.git','数据门户-h5':''}
+}
+const initProject = {
+    h5: "git@codeup.aliyun.com:xianfengsg/web/vue3-h5.git",
+    admin: 'git@codeup.aliyun.com:xianfengsg/web/vue3-admin.git'
+}
+
 const promptList = [
     {
         type: 'list',
@@ -29,24 +39,56 @@ const promptList = [
 ]
 
 program.version(require('../package.json').version, '-v,--version')
+program.command('project')
+    .alias('p')
+    .description('init project')
+    .action(() => {
+        const choices = Object.keys(project)
+        const list = [
+            {
+                type: 'list',
+                message: 'which menu you want',
+                name: 'menu',
+                default: choices[0],
+                choices: choices
+            }]
+
+        inquirer.prompt(list).then(async newAnswer => {
+            const { menu} = newAnswer
+            const choices1 = Object.keys(project[menu])
+            const newList = [{
+                type: 'list',
+                message: 'which project you want',
+                name: 'projectName',
+                default: choices1[0],
+                choices: choices1
+            }]
+            inquirer.prompt(newList).then(async project_name => {
+                const {projectName} = project_name
+                const url = project[menu][projectName]
+                await action(projectName, url, 'npm', false)
+            })
+        })
+    })
 program.command('init <name>')
     .description('init project')
+    .alias('i')
     .action(name => {
         inquirer.prompt(promptList).then(async answer => {
             const {template, tool} = answer
-            await action(name, template,tool)
+            await action(name, initProject[template], tool,true)
         })
     })
 
 program.command('page <name> [dirPath]')
     .description('insert page')
-    .action(async (name,dirPath)=>{
-        await insertRouter(name,dirPath)
+    .action(async (name, dirPath) => {
+        await insertRouter(name, dirPath)
     })
 program.command('component <name> [dirPath]')
     .description('insert page')
-    .action(async (name,dirPath)=>{
-        await insertComponent(name,dirPath)
+    .action(async (name, dirPath) => {
+        await insertComponent(name, dirPath)
     })
 program.parse(process.argv)
 
